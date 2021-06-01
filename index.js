7b2214303b2d303764a5f97145dfb4181a3c7d5a
Tharun Kumar Sugumar <68676466+Tharun-coder@users.noreply.github.com>
2021-06-01 10:58:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/helpers/connection");
-const steps = require("./src/routes/steps");
-const { Schema } = require("mongoose");
+const router = require("./src/routes/steps");
+
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/steps", steps);
+app.use("/steps", router);
 
 connectDB
   .then((port) => {
